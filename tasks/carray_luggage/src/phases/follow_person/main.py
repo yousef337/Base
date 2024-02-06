d3fe39645d9a2b516244a0d4d28c999b93ec2781
Yousef Altaher <yousef.a.altaher@gmail.com>
2024-02-06 22:15:40 +0000

    Sync to demo

--- tasks/carray_luggage/src/phases/follow_person/main.py
+++ tasks/carray_luggage/src/phases/follow_person/main.py
@@ -8,6 +8,10 @@ from geometry_msgs.msg import PointStamped, Point, Pose
 from sensor_msgs.point_cloud2 import read_points_list
 
 
+def get_straightest_angle(context, detections):
+    robot_pose = context.baseController.get_current_pose()
+    return min(map(lambda x: (x, np.arccos(np.dot(x[:2], robot_pose[:2])/(np.linalg.norm(x[:2]) * np.linalg.norm(robot_pose[:2])))), detections), key=lambda x: abs(x[1]))
+
 def get_person_location(context):
 
     looks = [
@@ -16,8 +20,14 @@ def get_person_location(context):
         context.headController.look_right,
     ]
 
+    sleep = False
+
     for i in looks:
-        rospy.sleep(1)
+        if sleep:
+            rospy.sleep(1)
+        else:
+            sleep = True
+
         i()
 
         img_msg = rospy.wait_for_message('/xtion/rgb/image_raw', Image)
@@ -25,18 +35,25 @@ def get_person_location(context):
             '/xtion/depth_registered/points', PointCloud2
         )
         detections = list(
-            filter(
-                lambda x: x.name in ['person'],
-                context.yolo(
-                    img_msg, 'yolov8n-seg.pt', 0.7, 0.2
-                ).detected_objects,
+            filter(lambda a: a is not None,
+            map(
+                lambda x: estimate_xyz_from_points(
+                    context, pcl_msg, contour(context, img_msg, np.array(x.xyseg).reshape(-1, 2)).tolist()
+                ),
+                list(filter(
+                    lambda x: x.name in ['person'],
+                    context.yolo(
+                        img_msg, 'yolov8n-seg.pt', 0.7, 0.2
+                    ).detected_objects,
+                ))
+            )
             )
         )
-
-        if detections and detections[0]:
-            return estimate_xyz_from_points(
-                context, pcl_msg, contour(context, img_msg, np.array(detections[0].xyseg).reshape(-1, 2)).tolist()
-        )
+        
+        if detections and detections[0].any():
+            return get_straightest_angle(
+                context, detections
+                )[0]
 
     return []
 
@@ -70,10 +87,10 @@ def estimate_xyz_from_points(context, pcl_msg, points, c=0):
     except:
         if c < 10:
             print(c)
-            rospy.sleep(1)
             return estimate_xyz_from_points(context, pcl_msg, points, c + 1)
         else:
-            raise MemoryError()
+            # raise MemoryError()
+            return None
 
 def to_map_frame(context, pcl_msg, pose):
     centroid_xyz = pose
@@ -94,6 +111,28 @@ def to_map_frame(context, pcl_msg, pose):
     )
 
 
+def marker(x, y):
+    from visualization_msgs.msg import Marker
+
+    marker_pub = rospy.Publisher("/visualization_marker", Marker, queue_size=2)
+    marker = Marker()
+    marker.header.stamp = rospy.Time.now()
+    marker.header.frame_id = "/map"
+    marker.type = 8
+    marker.color.r = 0.0
+    marker.color.g = 1.0
+    marker.color.b = 0.0
+    marker.color.a = 1.0
+    marker.scale.x = 0.1
+    marker.scale.y = 0.1
+    marker.scale.z = 0.1
+
+    marker.pose.position.x = x
+    marker.pose.position.y = y
+    marker.pose.orientation.w = 1
+    marker_pub.publish(marker)
+
+
 def main(context):
     last_person_pose = get_person_location(context)[:2]
     robot_pose = context.baseController.get_current_pose()
@@ -101,41 +140,37 @@ def main(context):
     print(robot_pose[:2])
     stand_still = 0
     
-    while stand_still < 10:
-
+    while stand_still < 50:
+        print(last_person_pose)
+        print([robot_pose[0], robot_pose[1]])
         if (
             len(last_person_pose) > 0
-            and np.linalg.norm(np.array([robot_pose[0], robot_pose[1]]) - last_person_pose) > 2
+            and np.linalg.norm(np.array([robot_pose[0], robot_pose[1]]) - last_person_pose) > 1
         ):
 
-            print('====================NORM')
-            print(np.linalg.norm(np.array([robot_pose[0], robot_pose[1]]) - last_person_pose))
-
-
+            ori = context.baseController.compute_face_quat(last_person_pose[0], last_person_pose[1]).orientation
             p = Pose()
             p.position.x = last_person_pose[0]
             p.position.y = last_person_pose[1]
-            p.orientation.x = robot_pose[2].x
-            p.orientation.y = robot_pose[2].y
-            p.orientation.z = robot_pose[2].z
-            p.orientation.w = robot_pose[2].w
+            p.orientation = ori
 
-            if abs(last_person_pose[0] - robot_pose[0]) > 2:
-                p.position.x -= 1.5 if last_person_pose[0] > robot_pose[0] else -1.5
+
+            if abs(last_person_pose[0] - robot_pose[0]) > 1:
+                p.position.x -= 1 if last_person_pose[0] > robot_pose[0] else -1
             
-            if abs(last_person_pose[1] - robot_pose[1]) > 2:
-                p.position.y -= 1.5 if last_person_pose[1] > robot_pose[1] else -1.5
+            if abs(last_person_pose[1] - robot_pose[1]) > 1:
+                p.position.y -= 1 if last_person_pose[1] > robot_pose[1] else -1
             
-            context.baseController.sync_to_pose(p)
-            context.baseController.sync_to_pose(p)
+            rospy.sleep(0.5)
+            context.baseController.async_to_pose(p)
+            robot_pose = context.baseController.get_current_pose()
 
             stand_still = 0
         else:
             stand_still += 1
 
-        rospy.sleep(0.7)
+        # rospy.sleep(0.7)
         last_person_pose = get_person_location(context)[:2]
-        robot_pose = context.baseController.get_current_pose()
         if (len(last_person_pose) > 0):
             print('====================NORM')
             print(np.linalg.norm(np.array([robot_pose[0], robot_pose[1]]) - last_person_pose))
